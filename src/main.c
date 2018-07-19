d6f5a68794303240f20361d6551969b966ca5903
Richard Denton <isdampe@gmail.com>
2018-07-20 01:17:32 +1000

    ENHANCE: Accept command line parameters.

--- src/main.c
+++ src/main.c
@@ -4,18 +4,26 @@
 #include "scheduler.h"
 
 void usage() {
-	printf("Usage: ./brocard [num_threads]\n");
+	printf("Usage: ./brocard [num_threads] [max_number]\n");
 	exit(1);
 }
 
 int main(int argc, char **argv)
 {
+	int thread_count, max_number;
+	if (argc < 3)
+		usage();
 
-	if (argc < 2)
+	thread_count = atoi(argv[1]);
+	if (thread_count == 0)
+		usage();
+
+	max_number = atoi(argv[2]);
+	if (max_number == 0)
 		usage();
 
 	struct brocard_scheduler scheduler;
-	brocard_scheduler_init(&scheduler, 4, 1000000);
+	brocard_scheduler_init(&scheduler, thread_count, max_number);
 	brocard_scheduler_loop(&scheduler);
 	brocard_scheduler_free(&scheduler);
 	return 0;
