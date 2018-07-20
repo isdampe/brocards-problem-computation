eaebd07d22f80800f380fb94bf483ebe46d015e8
Richard Denton <isdampe@gmail.com>
2018-07-20 22:49:47 +1000

    BUILD: Add c99 std

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir -p bin/
-gcc -O2 -o bin/brocard src/*.c -lpthread -lgmp
+gcc -O2 -o bin/brocard src/*.c -lpthread -lgmp -std=c99
 gcc -O2 -o bin/brocard-timer src/brocard.c src/scheduler.c tests/timed.c -lpthread -lgmp
